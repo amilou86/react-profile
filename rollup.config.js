3310f95c68f72221c98e9818a789230130a1b38d
amilou86 <98551756+amilou86@users.noreply.github.com>
2024-03-30 09:19:28 +0000

    rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,5 +6,17 @@ export default {
         dir: 'output',
         format: 'cjs'
     },
-    plugins: [url()]
+    plugins: [
+        url({
+            // Specify which files to handle (e.g., PDF files)
+            include: ['**/*.pdf', '**/*.jpg', '**/*.jpeg', '**/*.png'],
+
+            // By default, this plugin will emit files into the output directory
+            // You can specify a different output directory if needed
+            // fileName: '[name][extname]',
+
+            // You can also configure options for handling the files, such as emitting to the output directory
+            // emitFiles: true, // Uncomment this if you want the plugin to emit the files to the output directory
+        })
+    ]
 };
